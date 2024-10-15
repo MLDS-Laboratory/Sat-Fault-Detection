53a64aa3085c05ec1a2ad6fb4ffa50a5389ce2aa
Varun Unnithan <varun.unnithan33@gmail.com>
2024-10-15 12:07:10 -0400

    PostgreSQL dockerfile (not tested)

--- simulation-container/setup.bat
+++ simulation-container/setup.bat
@@ -1,2 +1,2 @@
 docker build -t basilisk-simulation .
-docker run -it -v ${PWD}\simulations\:/basilisk/simulations/ --name basilisk_container basilisk-simulation
+docker run -it -v %CD%\simulations:/basilisk/simulations --name basilisk_container basilisk-simulation
