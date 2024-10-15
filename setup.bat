cc13eccc61eb914d30c835f4ce21706f96f7b9e2
Varun Unnithan <varun.unnithan33@gmail.com>
2024-10-15 02:51:54 -0400

    virtual binds mount

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
 docker build -t basilisk-simulation .
-docker run -it --name basilisk_container basilisk-simulation
+docker run -it -v ${PWD}:/basilisk/simulations/ --name basilisk_container basilisk-simulation
--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
 docker start basilisk_container
 docker exec -it basilisk_container /bin/bash
+source .venv/bin/activate
