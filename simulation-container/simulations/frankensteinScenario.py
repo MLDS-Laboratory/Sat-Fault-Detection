d018b959d76114bba03998aa3c14efdf76916041
Pranav Narayan <pranavnarayan26@gmail.com>
2025-02-26 21:07:49 -0500

    changed afault to rwfault - faults now modify rw max torques

--- simulation-container/simulations/frankensteinScenario.py
+++ simulation-container/simulations/frankensteinScenario.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from copy import copy
+from collections import OrderedDict
+import time
 
 #utilities?
 from Basilisk.architecture import messaging
@@ -10,11 +12,14 @@ from Basilisk.utilities import macros
 from Basilisk.utilities import orbitalMotion
 from Basilisk.utilities import unitTestSupport
 from Basilisk.utilities import RigidBodyKinematics as rbk
+from Basilisk.utilities import fswSetupRW
+from Basilisk.utilities import simIncludeRW
 
 #simulation tools
 
 from Basilisk.simulation import coarseSunSensor
-
+from Basilisk.simulation import reactionWheelStateEffector
+from Basilisk.simulation import motorVoltageInterface
 from Basilisk.simulation import spacecraft
 from Basilisk.simulation import extForceTorque
 from Basilisk.utilities import simIncludeGravBody
@@ -24,12 +29,14 @@ from Basilisk.simulation import eclipse
 from Basilisk.fswAlgorithms import attTrackingError
 from Basilisk.fswAlgorithms import inertial3D
 from Basilisk.fswAlgorithms import mrpFeedback
+from Basilisk.fswAlgorithms import rwMotorTorque
+from Basilisk.fswAlgorithms import rwMotorVoltage
 
 #general simulation initialization, i think
 from Basilisk.utilities import SimulationBaseClass
 
 
-def run(vfault, afault, sfault, plot):
+def run(vfault, rwfault, sfault, plot):
     #a bunch of initializations
     simTaskName = "sim city"
     simProcessName = "mr. sim"
@@ -62,7 +69,7 @@ def run(vfault, afault, sfault, plot):
     #orientation of body frame relative to inertial
     satellite.hub.sigma_BNInit = rbk.euler3212MRP([0, 0, 0])
     #ang velocity of body frame relative to inertial expressed in body frame coords
-    satellite.hub.omega_BN_BInit = [[0.0 * macros.D2R], [0.0], [2.00 * macros.D2R]] 
+    satellite.hub.omega_BN_BInit = [[0.0 * macros.D2R], [0.0], [0.0]]#[2.00 * macros.D2R]] 
 
     satSim.AddModelToTask(simTaskName, satellite)
 
@@ -123,7 +130,11 @@ def run(vfault, afault, sfault, plot):
     #attitude error from reference
     attError = attTrackingError.attTrackingError()
     attError.ModelTag = "attErrorInertial3D"
+    attError.attNavInMsg.subscribeTo(nav.attOutMsg)
+    attError.attRefInMsg.subscribeTo(inertial.attRefOutMsg)
     satSim.AddModelToTask(simTaskName, attError)
+
+    
 
     #eclipses
     #optionally can be used if desired, just uncomment eclipse line in setup
@@ -166,22 +177,73 @@ def run(vfault, afault, sfault, plot):
     sensors[5].nHat_B = np.array([0.0, 0.0, -1.0])
 
         
-   
+    #reaction wheels
+    rwFactory = simIncludeRW.rwFactory()
+    """
+    RW Model:
+
+    BalancedWheels: ideal RW, no imperfections or external effects
+    SimpleJitter: includes forces and undesirable torques induced by RW as external effects
+    JitterFullyCoupled: includes forces and desirable torques induced by RW as internal effects
+    """
+    rwModel = messaging.BalancedWheels
+
+    """
+    Available RW types (afaik) - Honeywell_HR16, Honeywell_HR14, Honeywell_HR12
+
+    Mandatory arguments: RW type, spin axis (body frame)
+    Select optional arguments: Omega (initial speed in RPM, Float), maxMomentum (max ang. momentum storage in Nms, Float), 
+        useRWfriction (Bool), useMinTorque (Bool), useMaxTorque (saturation point, Bool), 
+        rWB_B (RW COM relative to body frame in m, Float(3), label (String), u_max (max motor torque in N-m, Float)) 
+    
+    The Honeywells require defining maxMomentum, though each has unique acceptable values. 
+    """
+    u_max = 0.2
+    RWx = rwFactory.create("Honeywell_HR16", [1, 0, 0], maxMomentum=100., RWModel=rwModel, u_max=u_max)
+    RWy = rwFactory.create("Honeywell_HR16", [0, 1, 0], maxMomentum=100., RWModel=rwModel, u_max=u_max)
+    RWz = rwFactory.create("Honeywell_HR16", [0, 0, 1], maxMomentum=100., RWModel=rwModel, u_max=u_max)
+
+    numRW = rwFactory.getNumOfDevices()
+
+    #adding RWs to s/c and sim
+    rwEffector = reactionWheelStateEffector.ReactionWheelStateEffector()
+    rwEffector.ModelTag = "RW_cluster"
+    rwFactory.addToSpacecraft(satellite.ModelTag, rwEffector, satellite)
+    satSim.AddModelToTask(simTaskName, rwEffector, 2) #the 2 ensures it will get updated before the satellite - higher priority
 
     #control torque
+    """note: because of the RW's higher execution priority, 
+        this MUST be created before rwMotor in the code to ensure initialization of its cmdTorqueOutMsg"""
     control = mrpFeedback.mrpFeedback()
+    
     control.ModelTag = "mrpFeedback"
     satSim.AddModelToTask(simTaskName, control)
-    control.K = 1
-    control.Ki = 0.001 #negative turns integral control off
-    control.P = 5
+    #parameters taken from scenarioAttitudeFeedbackRW
+    control.K = 3.5
+    control.Ki = -1 #negative turns integral control off
+    control.P = 30.0
+    control.integralLimit = 2. / control.Ki * 0.1
 
+    #maps desired torque to RW, apparently
+    rwMotor = rwMotorTorque.rwMotorTorque()
+    rwMotor.ModelTag = "rwMotorTorque"
+    satSim.AddModelToTask(simTaskName, rwMotor)
+
+    #defines axes of control available to RWs (all 3, here)
+    controlAxes_B = [
+         1, 0, 0, 0, 1, 0, 0, 0, 1
+    ]
+    rwMotor.controlAxes_B = controlAxes_B
+
+    
+    """CURRENTLY UNUSED"""
+    #was formerly used to create external torques as "faults"
     #disturbance torque setup
     disturbance = extForceTorque.ExtForceTorque()
     disturbance.ModelTag = "externalDisturbance"
     satellite.addDynamicEffector(disturbance)
     satSim.AddModelToTask(simTaskName, disturbance)
-    disturbance.cmdTorqueInMsg.subscribeTo(control.cmdTorqueOutMsg)
+    #disturbance.cmdTorqueInMsg.subscribeTo(control.cmdTorqueOutMsg)#used to control satellite before RWs
     disturbance.extTorquePntB_B = [[0], [0], [0]]
 
     #some final module subscriptions
@@ -194,11 +256,20 @@ def run(vfault, afault, sfault, plot):
     control.guidInMsg.subscribeTo(attError.attGuidOutMsg)
     control.vehConfigInMsg.subscribeTo(configDataMsg)
 
-    #idk why this is here and not above
-    attError.attNavInMsg.subscribeTo(nav.attOutMsg)
-    attError.attRefInMsg.subscribeTo(inertial.attRefOutMsg)
+    #connecting RWs to mrpFeedback and motors
+    fswSetupRW.clearSetup()
+    for _, rw in rwFactory.rwList.items():
+            fswSetupRW.create(unitTestSupport.EigenVector3d2np(rw.gsHat_B), rw.Js, uMax=u_max/5)    
+    rwParamMsg = fswSetupRW.writeConfigMessage()
+    control.rwParamsInMsg.subscribeTo(rwParamMsg)
+    control.rwSpeedsInMsg.subscribeTo(rwEffector.rwSpeedOutMsg)
+    rwMotor.vehControlInMsg.subscribeTo(control.cmdTorqueOutMsg)
+    rwMotor.rwParamsInMsg.subscribeTo(rwParamMsg)
+    rwEffector.rwMotorCmdInMsg.subscribeTo(rwMotor.rwMotorTorqueOutMsg)
+
+   
     
-    #data collection
+    """data collection"""
 
     #how often each logger samples
     samplingTime = unitTestSupport.samplingTime(simTime, simulationTimeStep,\
@@ -228,14 +299,19 @@ def run(vfault, afault, sfault, plot):
     torqueLog = control.cmdTorqueOutMsg.recorder(samplingTime)
     satSim.AddModelToTask(simTaskName, torqueLog)
 
-    #external disturbance torques
-    disturbanceLog = disturbance.cmdTorqueInMsg.recorder(samplingTime)
-    satSim.AddModelToTask(simTaskName, disturbanceLog)
-
     #sun state; completely unnecessary
     sunLog = spice.planetStateOutMsgs[1].recorder(samplingTime)
     satSim.AddModelToTask(simTaskName, sunLog)
 
+    #RW recorders
+    rwLog = rwEffector.rwSpeedOutMsg.recorder(samplingTime)#rwOutMsgs[]
+    satSim.AddModelToTask(simTaskName, rwLog)
+    rwMotorLog = rwMotor.rwMotorTorqueOutMsg.recorder(samplingTime)
+    satSim.AddModelToTask(simTaskName, rwMotorLog)
+    rwTorqueLog = []
+    for i in range(numRW):
+        rwTorqueLog.append(rwEffector.rwOutMsgs[i].recorder(samplingTime))
+        satSim.AddModelToTask(simTaskName, rwTorqueLog[i])
 
     
     #set up the simulation
@@ -249,10 +325,11 @@ def run(vfault, afault, sfault, plot):
 
     #for plotting purposes
     sensedSun = []
-    disturbances = [] 
-    
+    global done 
+    done = True
     #self-explanatory
     def fault_addition():
+        global done
         rand = random.random()
         #random delta-v faults
         if vfault and rand < 0.001:
@@ -260,13 +337,21 @@ def run(vfault, afault, sfault, plot):
             velo = unitTestSupport.EigenVector3d2np(velRef.getState())
             velRef.setState([x + y for x, y in zip(velo, [1 * rand, 1 * rand, 0.25 * rand])])
 
-        #random external torques
-        if afault and rand < 0.001:
-            factor = 5
-            disturbance.extTorquePntB_B = [[(factor * random.random() - factor / 2.0) / 10 * 0],\
-                                           [(factor * random.random() - factor / 2.0) / 10 ],\
-                                           [(factor * random.random() - factor / 2.0) / 20 * 0]]
-        disturbances.append(disturbance.extTorquePntB_B)
+        #random RW torque limit changes
+        if rwfault and rand < 0.01: #0.001
+            newRWs = []
+            for i in rwFactory.rwList:
+                rand2 = random.random() * 100
+                spin = [sublist[0] for sublist in rwFactory.rwList[i].gsHat_B]
+                if rand2 < 10:
+                    newRWs.append((spin, u_max / rand2 if rand2 >= 1 else u_max * rand2))
+                else:
+                    newRWs.append((spin, rwFactory.rwList[i].u_max))
+            rwFactory.rwList = OrderedDict([])
+            rwEffector.ReactionWheelData = rwEffector.ReactionWheelData[:0]
+            for (spin, torque) in newRWs:
+                RW_new = rwFactory.create("Honeywell_HR16", spin, maxMomentum=100., RWmodel=rwModel, u_max=torque)
+                rwEffector.addReactionWheel(RW_new)
 
         #random CSS faults
         if sfault and rand < 0.01:
@@ -306,13 +391,12 @@ def run(vfault, afault, sfault, plot):
         
 
     #actual execution loop
+    reset = False
     while satSim.TotalSim.CurrentNanos < simTime:
         satSim.TotalSim.SingleStepProcesses()
         #msgs.append(stateReader)
         if satSim.TotalSim.CurrentNanos >= faultTime:
             fault_addition()
-        else:
-            disturbances.append([[0], [0], [0]])
         sensorSunPos()
 
     #collecting some of the data for plotting and returning
@@ -324,7 +408,7 @@ def run(vfault, afault, sfault, plot):
 
     pos = np.array(sat_pos[:] - earth_pos[:])
     velo = np.array(sat_velo[:] - earth_velo[:])
-    #^^ cause i decided to add the sun, which apparently fucks position vectors up
+    #^^ cause i decided to add the sun, which centers the inertial frame of these loggers at the sun
 
     sunPoint = np.array(navLog.vehSunPntBdy)
 
@@ -338,9 +422,7 @@ def run(vfault, afault, sfault, plot):
     
     #plotting
     if plot:
-        
         plt.close("all")
-
         if vfault:
             #plot position relative to planet
             plt.figure(1)
@@ -381,22 +463,39 @@ def run(vfault, afault, sfault, plot):
                 plt.xlabel('Time [orbits]')
                 plt.ylabel('Trajectory Differences [m]')
 
-        if afault:
-            #external torque generations
-            plt.figure(3)
-            for i in range(len(disturbances)):
-                pass#times.append(i)
-            disturbances = np.array(disturbances)
-            for i in range(3):
-                plt.plot(satLog.times() * macros.NANO2SEC / period, disturbances[:, i], label = rf"$\tau_{i+1}$")
-            plt.title("Disturbance Torques")
+        if rwfault:
+            #RW Wheel Speeds
+            plt.figure(4)
+            for i in range(numRW):
+                plt.plot(satLog.times() * macros.NANO2SEC / period, rwLog.wheelSpeeds[:, i], label = f"RW {i+1}")
+                plt.title("RW Speeds")
+                plt.legend()
+                plt.xlabel("Time [orbits]")
+                plt.ylabel("Angular Speed [rad/s]")
+
+            #mrpFeedback Desired Torque Outputs
+            plt.figure(5)
+            for i in range(numRW):
+                plt.plot(satLog.times() * macros.NANO2SEC / period, rwMotorLog.motorTorque[:, i], label=f'RW {i+1}')
+            plt.title("mrpFeedback Desired Torques")
             plt.legend()
             plt.xlabel("Time [orbits]")
             plt.ylabel("Torque [N-m]")
 
+            #RW motor actual torques
+            motorTorque = [rw.u_current for rw in rwTorqueLog]
+            plt.figure(6)
+            for i in range(numRW):
+                plt.plot(satLog.times() / period, motorTorque[i], label=f'RW {i+1}')
+            plt.title("RW Motor - Torque Applied")
+            plt.legend()
+            plt.xlabel("Time [orbits]")
+            plt.ylabel("Torque [N-m]")
+            plt.ylim(-1.1 * u_max, 1.1 * u_max)
+
 
         #pointing vector to the sun in the body frame
-        plt.figure(4)
+        plt.figure(7)
         timeAxis = navLog.times()
         for i in range(3):
             plt.plot(timeAxis * macros.NANO2SEC / period, sunPoint[:, i],
@@ -408,7 +507,7 @@ def run(vfault, afault, sfault, plot):
         plt.ylabel("Vector Component")
 
         #CSS sensor values, biases included
-        plt.figure(5, figsize=(10,6))
+        plt.figure(8, figsize=(10,6))
         timeAxis = loggers[0].times()
         for i in range(len(CSSdata)):
             plt.subplot(2, 3, i+1)
@@ -422,7 +521,7 @@ def run(vfault, afault, sfault, plot):
 
         if sfault:
             #where the sensors collectively think the sun is (orientation vector)
-            plt.figure(6)
+            plt.figure(9)
             #not sure what i was thinking when i made this plot
             sensedSun = np.array(sensedSun)
             plt.plot(satLog.times () * macros.NANO2SEC / period, sensedSun[:, 0], label=rf"$\sigma_{1}$")
@@ -434,7 +533,7 @@ def run(vfault, afault, sfault, plot):
             plt.ylabel("Orientation (rad)")
 
         #satellite orientation relative to inertial
-        plt.figure(7)
+        plt.figure(10)
         for i in range(3):
             plt.plot(navLog.times() / period, sigma[:, i], label=rf"$\sigma_{i+1}$")
         plt.title("Inertial Orientation")
@@ -442,11 +541,15 @@ def run(vfault, afault, sfault, plot):
         plt.ylabel("Orientation (MRP)")
         plt.legend()
 
+        
+
         plt.tight_layout()
         plt.show()
 
-    return satLog.times(), pos, velo, sigma, omega, CSSdata, disturbances, sensedSun, sunPoint
+        
+    """MODIFY"""
+    return satLog.times(), pos, velo, sigma, omega, CSSdata, motorTorque, sensedSun, sunPoint
 
 if __name__ == "__main__":
-    #vfault, afault, sfault, plot
-    run(False, False, False, False)
+    #vfault, rwfault, sfault, plot
+    run(False, True, False, True)
