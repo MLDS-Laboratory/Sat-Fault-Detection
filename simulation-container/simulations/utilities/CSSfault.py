5f7c2b8f6d019764d08dae384188b3ec326aa50a
Pranav Narayan <pranavnarayan26@gmail.com>
2025-04-22 18:09:31 -0400

    CSSfault + simpleCSSFaultScenario built and tested

--- simulation-container/simulations/utilities/CSSfault.py
+++ simulation-container/simulations/utilities/CSSfault.py
@@ -1,27 +1,80 @@
-from fault import Fault
+from utilities.fault import Fault
 from collections.abc import Collection
 from collections import OrderedDict
 from typing import Any
 import random
 from Basilisk.architecture import sysModel, messaging
+from Basilisk.simulation import coarseSunSensor
 import numpy as np
 
 class CSSfault(sysModel.SysModel):
     def __init__(self, components : Collection[Any], **kwargs):
         defaults = kwargs.get("defaults")
         self.components = components
-        self.fault = Fault(components, defaults)
-        sysModel.SysModel.__init__()
+        self.defaults = defaults
+        self.types = ["OFF", "STUCK_CURRENT", "STUCK_MAX", "STUCK_RAND", "RAND", "NOMINAL"]
+        super().__init__()
+        self.faultState = ["NOMINAL"] * len(self.components)
+        self.chance = kwargs.get("chance")
+        self.seed = kwargs.get("seed")
+        for i in self.components:
+            i.faultState = getattr(coarseSunSensor, "NOMINAL")
         
+    def UpdateState(self, CurrentSimNanos):
+        self.faultState = [i[1] for i in self.inject_random(self.chance)]
+    def inject(self, newSettings : list):
+        for i in range(len(self.components)):           
+            try:
+                if newSettings[i].upper() == "NOMINAL":
+                    self.components[i].faultState = getattr(coarseSunSensor, (newSettings[i]).upper())
+                else:
+                    self.components[i].faultState = getattr(coarseSunSensor, "CSSFAULT_" + (newSettings[i]).upper())
+            except:
+                raise NameError(newSettings[i] + " is not yet a supported fault type.")
 
-    def inject(self, newSettings : list[dict]):
-        for i in range(len(self.components)):
-            eval("self." + newSettings["type"])
         return newSettings
 
     def inject_random(self, chance):
-        return super().inject_random(chance)
+        if not chance:
+            chance = 0.0005
+        toInject = self.randomize(chance, seed=self.seed)
+        newSettings = []
+        for i in range(len(toInject)):
+            if toInject[i]:
+                fault = np.random.choice(self.types, 1)[0]
+                newSettings.append(fault)
+            else:
+                past = self.components[i].faultState
+                if past == 4:
+                    newSettings.append("NOMINAL")
+                else:
+                    newSettings.append(self.types[self.components[i].faultState])
+        return list(zip(toInject, self.inject(newSettings)))
     
     def reset(self):
-        return super().reset()
+        if not self.defaults:
+            raise ValueError("Defaults have not been set.")
+        for i in range(len(self.components)):
+            try:
+                self.components[i].faultState = getattr(coarseSunSensor, "CSSFAULT_" + (self.defaults[i]).upper())
+            except:
+                raise TypeError("Defaults and/or Components do not contain necessary information.")
+    
+    def randomize(self, chance : float, **kwargs):
+        """
+        Choose which components to randomly have faults injected. 
+
+        Parameter:
+            chance (float): the chance that each component will fault.
+
+        Returns:
+            toInject (list[bool]): Indicates whether a fault is to be injected to the component at this index 
+        """
+        if not 0. <= chance <= 1.:
+            raise ValueError("Chance must be between 0 and 1.")
+        seed = kwargs.get("seed")
+        if seed is not None:
+            random.seed(seed)
+        toInject = [random.random() < chance for _ in self.components]
+        return toInject
     
