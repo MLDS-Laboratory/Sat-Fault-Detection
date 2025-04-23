98de1f20f4977bf1150e32eeb0df41c23fa10201
Pranav Narayan <pranavnarayan26@gmail.com>
2025-04-22 21:03:01 -0400

    deleted abstract class fault; modified rwfault to run as a Basilisk module; comments and style

--- simulation-container/simulations/utilities/CSSfault.py
+++ simulation-container/simulations/utilities/CSSfault.py
@@ -6,34 +6,73 @@ import random
 from Basilisk.architecture import sysModel, messaging
 from Basilisk.simulation import coarseSunSensor
 import numpy as np
+"""
+This is a module for introducing faults into Coarse Sun Sensors. It is a Basilisk Module, so it will
+run at every time step provided it is added to a simulation task. 
 
+The module is essentially a way to randomize the built-in faulted states of the coarseSunSensor module. 
+It randomly assigns random faults to each CSS. If the randomly chosen fault is anything except NOMINAL
+and RAND, that sensor is faulted until it gets a different randomly chosen fault. If the randomly chosen fault is NOMINAL, 
+that sensor is not faulted! If the randomly chosen fault is RAND, that sensor returns to NOMINAL the next turn, 
+and does not have an entry in the fault sweepstakes that turn.  
+"""
 class CSSfault(sysModel.SysModel):
+    """
+    Requires:
+    - A list of the sensors
+    Optional:
+    - A list of default fault states and other things for the sensors
+    - a chance for the random fault injections
+    - a seed for the random fault injections
+    """
     def __init__(self, components : Collection[Any], **kwargs):
         defaults = kwargs.get("defaults")
         self.components = components
         self.defaults = defaults
         self.types = ["OFF", "STUCK_CURRENT", "STUCK_MAX", "STUCK_RAND", "RAND", "NOMINAL"]
         super().__init__()
-        self.faultState = ["NOMINAL"] * len(self.components)
+        #loggable attribute for fault states
+        self.faultState = ["NOMINAL"] * len(self.components) #all sensors are assumed to start off nominal
         self.chance = kwargs.get("chance")
         self.seed = kwargs.get("seed")
         for i in self.components:
-            i.faultState = getattr(coarseSunSensor, "NOMINAL")
-        
+            i.faultState = getattr(coarseSunSensor, "NOMINAL") #forcibly make all sensors nominal at first
+    """
+    Update method. Runs at every timestep. 
+
+    Just calls the random fault injection and uses its results to update the loggable attribute. 
+    """
     def UpdateState(self, CurrentSimNanos):
         self.faultState = [i[1] for i in self.inject_random(self.chance)]
+    
+    """
+    Injection if specific new settings are desired. 
+
+    Goes through the list of sensors and sets them to the desired fault state. 
+    Used by inject_random once new settings have been randomized. Returns the
+    new settings. 
+    """
     def inject(self, newSettings : list):
         for i in range(len(self.components)):           
             try:
+                #for some reason, all the predefined fault states are CSSFAULT_something except NOMINAL
                 if newSettings[i].upper() == "NOMINAL":
                     self.components[i].faultState = getattr(coarseSunSensor, (newSettings[i]).upper())
                 else:
                     self.components[i].faultState = getattr(coarseSunSensor, "CSSFAULT_" + (newSettings[i]).upper())
             except:
                 raise NameError(newSettings[i] + " is not yet a supported fault type.")
-
         return newSettings
+    """
+    Random injection of faults with a given chance. 
 
+    Whether each sensor is faulted is randomized, and the type of fault is also
+    randomized. As stated above, if the fault chosen is RAND, it will only last 1 turn, 
+    and return to NOMINAL the next. Otherwise, it will remain at the chosen or prior state 
+    until changed here. This returns a list of the following form:
+    (bool, fault state) where bool is whether a fault is injected into the given component
+    and fault state is the new (or old, if no new fault) setting. 
+    """
     def inject_random(self, chance):
         if not chance:
             chance = 0.0005
@@ -41,9 +80,11 @@ class CSSfault(sysModel.SysModel):
         newSettings = []
         for i in range(len(toInject)):
             if toInject[i]:
+                #randomly choose the fault type
                 fault = np.random.choice(self.types, 1)[0]
                 newSettings.append(fault)
             else:
+                #check if the last fault state was RAND, and change back to NOMINAL if so
                 past = self.components[i].faultState
                 if past == 4:
                     newSettings.append("NOMINAL")
@@ -51,6 +92,12 @@ class CSSfault(sysModel.SysModel):
                     newSettings.append(self.types[self.components[i].faultState])
         return list(zip(toInject, self.inject(newSettings)))
     
+    """
+    Resets all reaction wheels to the prescribed defaults, if prescribed. 
+
+    Will eventually need to be updated to reset to NOMINAL if not prescribed, 
+    but doesn't really matter because this is not used in normal operation. 
+    """
     def reset(self):
         if not self.defaults:
             raise ValueError("Defaults have not been set.")
@@ -59,17 +106,16 @@ class CSSfault(sysModel.SysModel):
                 self.components[i].faultState = getattr(coarseSunSensor, "CSSFAULT_" + (self.defaults[i]).upper())
             except:
                 raise TypeError("Defaults and/or Components do not contain necessary information.")
-    
+    """
+    Choose which components to randomly have faults injected. 
+
+    Parameter:
+        chance (float): the chance that each component will fault.
+
+    Returns:
+        toInject (list[bool]): Indicates whether a fault is to be injected to the component at this index 
+    """
     def randomize(self, chance : float, **kwargs):
-        """
-        Choose which components to randomly have faults injected. 
-
-        Parameter:
-            chance (float): the chance that each component will fault.
-
-        Returns:
-            toInject (list[bool]): Indicates whether a fault is to be injected to the component at this index 
-        """
         if not 0. <= chance <= 1.:
             raise ValueError("Chance must be between 0 and 1.")
         seed = kwargs.get("seed")
--- simulation-container/simulations/utilities/rwfault.py
+++ simulation-container/simulations/utilities/rwfault.py
@@ -1,27 +1,86 @@
-from utilities.fault import Fault
+
 from collections.abc import Collection
 from collections import OrderedDict
 from typing import Any
 import random
-from Basilisk import messaging
+from Basilisk.architecture import messaging, sysModel
 import numpy as np
+"""
+This is a module for introducing faults into Reaction Wheels. It is a Basilisk module,
+so it runs at every timestep provided it is added to a simulation task. 
 
-class RWFault(Fault):
+The only implemented methodology for faults thus far is a reduction in max torque output 
+of the reaction wheels. The introduction of friction will eventually be added. 
+"""
+class RWFault(sysModel.SysModel):
+    """
+    Requires, in order:
+      - A list of the form (type, RW), where type is the model of the reaction wheel and RW is the reaction wheel itself
+      - the rwFactory (simIncluderwFactory()) used to generate those wheels
+      - the rwEffector (reactionWheelStateEffector) used to make the wheels actually do something
+    
+    Optional:
+      - the model used for the RWs (jitter, balanced, etc). not not implemented yet
+      - a seed for the randomized fault injection
+      - a fault injection chance per RW for the randomized fault injection
+      - defaults - the original settings of the RWs
+    """
     def __init__(self, components: Collection[Any], rwFactory, rwEffector, **kwargs):
-        super().__init__(components, **kwargs)
+        super().__init__()
+        self.components = components
         self.rwFactory = rwFactory
         self.rwEffector = rwEffector
         self.rwModel = kwargs.get("model")
         self.seed = kwargs.get("seed")
+        self.chance = kwargs.get("chance")
+        self.defaults = kwargs.get("defaults")
+        #if the defaults are not passed in, the defaults are pulled from the starting states
+        if not self.defaults:
+            self.defaults = []
+            for i in range(len(components)):
+                spin = [sublist[0] for sublist in self.components[i][1].gsHat_B]
+                rwType = self.components[i][0]
+                u_max = self.components[i][1].u_max
+                self.defaults.append({"axis":spin, "u_max":u_max, "rwType":rwType})
+        #loggable attribute for the actual traits of the RWs
+        self.state = [(i["rwType"], i["axis"], i["u_max"]) for i in self.defaults]
+        #loggable attribute for the timesteps of fault injections
+        self.fault = [False for _ in self.components]
+
+    """
+    Update method. Runs every timestep. 
+
+    Just calls the inject_random method, and updates the loggable attributes with the results. 
+    """
+    def UpdateState(self, CurrentSimNanos):
+        state = self.inject_random(self.chance)
+        self.state = state[:, 1]
+        self.fault = state[:, 0]
     
+
+    """
+    The method that actually changes out the RWs. 
+
+    Only called from inject() - takes in new RW settings, and uses them to replace
+    the batch of RWs that existed previously with new ones with those settings.
+
+    This is the only way I could find to introduce faults into RWs on the Python side. 
+    """
     def replaceRWs(self, newRWs):
+        #empty existing RW carriers
         self.rwFactory.rwList = OrderedDict([])
         self.rwEffector.ReactionWheelData = self.rwEffector.ReactionWheelData[:0]
+        newComps = []
+        #create new RWs
         for (rwType, spin, torque) in newRWs:
             rwModel = self.rwModel if self.rwModel else messaging.BalancedWheels
             RW_new = self.rwFactory.create(rwType, spin, maxMomentum=100., RWmodel=rwModel, u_max=torque)
             self.rwEffector.addReactionWheel(RW_new)
-
+            newComps.append((rwType, RW_new))
+        self.components = newComps
+    """
+    Resets all RWs to default settings. 
+    """
     def reset(self):
         if not self.defaults:
             raise ValueError("Defaults have not been set.")
@@ -37,26 +96,62 @@ class RWFault(Fault):
         self.replaceRWs(resetRWs)
         return len(self.rwFactory.rwList) > 0 and len(self.rwEffector.ReactionWheelData) > 0
 
+    """
+    Meant for non-random injections where the new desired settings are known. Not used in practice, 
+    yet, except as a bridge between inject_random and replaceRWs. 
+    """
     def inject(self, newSettings) -> Collection[Any]:
         self.replaceRWs(newSettings)
         return newSettings
+    """
+    Does the bulk of the injections in practice currently, randomized. 
 
+    Each RW has the same chance of being faulted. If it isn't faulted, 
+    the RW still will be replaced, but with the same settings. 
+    If it is faulted, the RW's max torque output will be reduced by some 
+    random factor. This may become multiplicative. 
+
+    This method returns a list of tuples of the form:
+    (bool, (rwType, spin axis, torque)) 
+    where bool is whether a fault was injected, rwType is the rwModel, and torque is the max torque output. 
+    """
     def inject_random(self, chance : float) -> Collection[tuple[bool, Any]]:
+        #if chance isn't passed in
+        if not chance:
+            chance = 0.001
         toInject = self.randomize(chance, seed=self.seed)
         if self.seed:
             random.seed(self.seed)
         newSettings = []
         for i in range(len(toInject)):
+            #spin and rwType stay the same
             spin = [sublist[0] for sublist in self.components[i][1].gsHat_B]
             rwType = self.components[i][0]
+            #u_max is changed if there's a fault by a random factor
             u_max = self.components[i][1].u_max
             if toInject[i]:
                 rand = random.random() * 10
                 u_max = u_max / rand if rand >= 1 else u_max * rand
             newSettings.append((rwType, spin, u_max))
         details = list(zip(toInject, self.inject(newSettings)))
-        for i in range(len(details)):
-            if not details[i][0]:
-                details[i] = (details[i][0], None)
         return np.array(details, dtype=object)
+    
+    """
+    Choose which components to randomly have faults injected. 
+
+    Parameter:
+        chance (float): the chance that each component will fault.
+
+    Returns:
+        toInject (list[bool]): Indicates whether a fault is to be injected to the component at this index 
+    """
+    def randomize(self, chance : float, **kwargs):
+
+        if not 0. <= chance <= 1.:
+            raise ValueError("Chance must be between 0 and 1.")
+        seed = kwargs.get("seed")
+        if seed is not None:
+            random.seed(seed)
+        toInject = [random.random() < chance for _ in self.components]
+        return toInject
         
